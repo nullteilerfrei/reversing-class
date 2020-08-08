a5bb875f09275cab755d63f624f4a685ebfd82d0
Lars Wallenborn <lars@wallenborn.net>
2020-08-08 20:06:31 +0200

    script to clear data

---
+++ scripts/java/cts.java
@@ -0,0 +1,19 @@
+//Clear This Shit - Clears data of given size starting at the cursor
+//@author @larsborn
+//@category Helper
+//@keybinding 
+//@menupath 
+//@toolbar 
+
+import ghidra.app.script.GhidraScript;
+import ghidra.program.model.address.*;
+
+public class cts extends GhidraScript {
+
+	public void run() throws Exception {
+		int clearSize = askInt("Enter Size",
+				String.format("Size of area to clear (starting at %08x)", currentAddress.getOffset()));
+		Address clearEnd = toAddr(currentAddress.getOffset() + clearSize);
+		this.clearListing(currentAddress, clearEnd);
+	}
+}
