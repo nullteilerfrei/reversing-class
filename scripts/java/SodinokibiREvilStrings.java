5c7d217674e4bdeb3a764a7b93ff3cb1ba8c8352
Lars Wallenborn <lars@wallenborn.net>
2021-08-01 15:20:29 +0200

    some refactoring and rename variables

--- scripts/java/SodinokibiREvilStrings.java
+++ scripts/java/SodinokibiREvilStrings.java
@@ -13,6 +13,8 @@ import ghidra.app.decompiler.DecompInterface;
 import ghidra.app.decompiler.DecompileResults;
 import ghidra.app.script.GhidraScript;
 import ghidra.util.exception.CancelledException;
+import ghidra.util.exception.DuplicateNameException;
+import ghidra.util.exception.InvalidInputException;
 import ghidra.program.model.symbol.*;
 import ghidra.program.model.mem.*;
 import ghidra.program.model.listing.*;
@@ -21,7 +23,6 @@ import ghidra.program.database.mem.FileBytes;
 import ghidra.program.model.address.*;
 
 public class SodinokibiREvilStrings extends GhidraScript {
-
 	public void run() throws Exception {
 		String deobfuscatorName;
 		try {
@@ -30,37 +31,68 @@ public class SodinokibiREvilStrings extends GhidraScript {
 		} catch (CancelledException X) {
 			return;
 		}
-		OUTER_LOOP: for (Address callAddr : getCallAddresses(deobfuscatorName)) {
+		for (Address callAddr : getCallAddresses(deobfuscatorName)) {
 			monitor.setMessage(String.format("parsing call at %08X", callAddr.getOffset()));
-
-			int arguments[] = { 1, 2, 3, 4 };
-			OptionalLong options[] = getConstantCallArgument(callAddr, arguments);
-			for (OptionalLong option : options) {
-				if (option.isEmpty()) {
-					println(String.format("Argument to call at %08X is not a constant string.", callAddr.getOffset()));
-					continue OUTER_LOOP;
-				}
-			}
-
-			long blobAddress = options[0].getAsLong();
-			int keyOffset = (int) options[1].getAsLong();
-			int keyLength = (int) options[2].getAsLong();
-			int dataLength = (int) options[3].getAsLong();
-			if (dataLength == 0 || keyLength == 0)
-				continue;
-
-			byte[] key = getOriginalBytes(toAddr(blobAddress + keyOffset), keyLength);
-			byte[] data = getOriginalBytes(toAddr(blobAddress + keyOffset + keyLength), dataLength);
-			byte[] decrypted = new RC4(key).encrypt(data);
-
-			String deobfuscated = AsciiDammit(decrypted, dataLength);
-			println(String.format("%08X : %s", callAddr.getOffset(), deobfuscated));
-			setComment(callAddr, String.format("Deobfuscated: %s", deobfuscated));
-			createBookmark(callAddr, "DeobfuscatedString", deobfuscated);
+			deobfuscateStringForSingleCall(callAddr);
 		}
 	}
 
-	private String AsciiDammit(byte[] data, int len) {
+	private void deobfuscateStringForSingleCall(Address callAddr) throws Exception {
+		int arguments[] = { 1, 2, 3, 4, 5 };
+		OptionalLong options[] = getConstantCallArgument(callAddr, arguments);
+		if (options[0].isEmpty() || options[1].isEmpty() || options[2].isEmpty() || options[3].isEmpty()) {
+			println(String.format("Argument to call at %08X is not a constant string.", callAddr.getOffset()));
+			return;
+		}
+
+		long blobAddress = options[0].getAsLong();
+		long offset = options[1].getAsLong();
+		long keySize = options[2].getAsLong();
+		long stringSize = options[3].getAsLong();
+		if (stringSize == 0 || keySize == 0) {
+			return;
+		}
+
+		byte[] deobfuscatedData = deobfuscate(blobAddress, offset, keySize, stringSize);
+		// hexdump(deobfuscatedData);
+
+		String deobfuscatedString = asciiDammit(deobfuscatedData, (int) stringSize);
+		makeDataAvailableInUserInterface(callAddr, deobfuscatedString,
+				options[4].isEmpty() ? 0 : options[4].getAsLong());
+	}
+
+	private byte[] deobfuscate(long blobAddress, long offset, long keySize, long stringSize) {
+		byte[] key = getOriginalBytes(toAddr(blobAddress + offset), (int) keySize);
+		byte[] data = getOriginalBytes(toAddr(blobAddress + offset + keySize), (int) stringSize);
+		return new RC4(key).decrypt(data);
+	}
+
+	public void makeDataAvailableInUserInterface(Address callAddr, String deobfuscatedString, long resultAddr) {
+		println(String.format("%08X : %s", callAddr.getOffset(), deobfuscatedString));
+		setComment(callAddr, String.format("Deobfuscated: %s", deobfuscatedString));
+		createBookmark(callAddr, "DeobfuscatedString", deobfuscatedString);
+		if (resultAddr > 0) {
+			Address address = currentAddress.getNewAddress(resultAddr);
+			Function function = getFunctionContaining(callAddr);
+			StackFrame stackFrame = function.getStackFrame();
+			for (Variable variable : stackFrame.getLocals()) {
+				if (variable.getStackOffset() == (int) address.getOffset()) {
+					try {
+						variable.setName(sanitize(deobfuscatedString),
+								ghidra.program.model.symbol.SourceType.USER_DEFINED);
+					} catch (DuplicateNameException | InvalidInputException e) {
+						println("Variable renaming failed for " + deobfuscatedString);
+					}
+				}
+			}
+		}
+	}
+
+	private String sanitize(String s) {
+		return s.replace("\\", "_").replace(" ", "-").replace("\n", "_").replace("\t", "_").replace("\r", "_");
+	}
+
+	public String asciiDammit(byte[] data, int len) {
 		boolean isWide = true;
 		byte[] nonWide = new byte[len / 2];
 		for (int i = 0; i < len / 2; i++) {
@@ -174,5 +206,85 @@ public class SodinokibiREvilStrings extends GhidraScript {
 
 	private void setCommentToDecompiledCode(Address address, String comment) {
 		currentProgram.getListing().getCodeUnitAt(address).setComment(CodeUnit.PRE_COMMENT, comment);
+	}
+
+	private void hexdump(byte[] data) {
+		int lineWidth = 16;
+		for (int i = 0; i < Math.ceil((double) data.length / lineWidth); i++) {
+			int offset = i * lineWidth;
+			// address
+			print(String.format("%08x  ", offset));
+
+			// actual hex dump
+			for (int j = 0; j < lineWidth; j++) {
+				if (lineWidth / 2 == j) {
+					print(" ");
+				}
+				if (offset + j < data.length) {
+					print(String.format("%02x ", data[offset + j]));
+				} else {
+					print("   ");
+				}
+			}
+
+			// printable
+			print(" |");
+			for (int j = 0; j < lineWidth; j++) {
+				if (offset + j < data.length) {
+					if (isPrintable(data[offset + j])) {
+						print(String.format("%c", data[offset + j]));
+					} else {
+						print(".");
+					}
+				}
+			}
+			print("|\n");
+		}
+	}
+
+	private boolean isPrintable(byte c) {
+		return !(c < 0x20 || c > 127);
+	}
+
+	private class RC4 {
+		private final byte[] S = new byte[256];
+		private final byte[] T = new byte[256];
+
+		public RC4(final byte[] key) {
+			if (key.length < 1 || key.length > 256) {
+				throw new IllegalArgumentException("invalid key length");
+			}
+			for (int i = 0; i < 0x100; i++) {
+				S[i] = (byte) i;
+				T[i] = key[i % key.length];
+			}
+			int j = 0;
+			for (int i = 0; i < 0x100; i++) {
+				j = (j + S[i] + T[i]) & 0xFF;
+				swap(i, j);
+			}
+		}
+
+		public byte[] encrypt(final byte[] plainText) {
+			final byte[] cipherText = new byte[plainText.length];
+			int i = 0, j = 0;
+			for (int pos = 0; pos < plainText.length; pos++) {
+				i = (i + 1) & 0xFF;
+				j = (j + S[i]) & 0xFF;
+				swap(i, j);
+				cipherText[pos] = (byte) (plainText[pos] ^ S[(S[i] + S[j]) & 0xFF]);
+			}
+			return cipherText;
+		}
+
+		public byte[] decrypt(final byte[] cipherText) {
+			return encrypt(cipherText);
+		}
+
+		private void swap(int i, int j) {
+			byte tmp = S[j];
+			S[j] = S[i];
+			S[i] = tmp;
+		}
 	}
 }
